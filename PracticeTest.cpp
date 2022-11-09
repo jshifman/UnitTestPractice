baae16f5023cbbb61260235ba18c8b0b76c2b632
jshifman <josephshifman@gmail.com>
2022-11-09 11:49:23 -0800

    assignment

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -14,7 +14,46 @@ class PracticeTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
-TEST(PracticeTest, smoke_test)
+TEST(PracticeTest, no_allnighters)
 {
-    ASSERT_TRUE(true);
+    Practice mypractice;
+	int nights[7] = {1,1,1,1,1,1,1};
+	int* result = mypractice.allnighter(nights);
+	ASSERT_TRUE(result == nullptr);
+}
+
+TEST(PracticeTest, all_allnighters)
+{
+    Practice mypractice;
+	int nights[7] = {0,0,0,0,0,0,0};
+	int* result = mypractice.allnighter(nights);
+	int p = *result;
+	ASSERT_EQ(p, 0);
+}
+
+TEST(PracticeTest, secondday_allnighter)
+{
+    Practice mypractice;
+	int nights[7] = {1,0,1,1,1,1,1};
+	int* result = mypractice.allnighter(nights);
+	int p = *result;
+	ASSERT_EQ(p, 0);
+}
+
+TEST(PracticeTest, thirdday_allnighter)
+{
+    Practice mypractice;
+	int nights[7] = {1,1,0,1,1,1,1};
+	int* result = mypractice.allnighter(nights);
+	int p = *result;
+	ASSERT_EQ(p, 0);
+}
+
+TEST(PracticeTest, fourthday_allnighter)
+{
+    Practice mypractice;
+	int nights[7] = {1,1,1,0,1,1,1};
+	int* result = mypractice.allnighter(nights);
+	int p = *result;
+	ASSERT_EQ(p, 0);
 }
